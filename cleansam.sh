49f79f95e4f38c388c11182581b2a85eb892c463
Ozan Kiratli <ozankiratli@protonmail.com>
2020-12-08 16:41:09 -0500

    Minor change to add to serial preprocessing

--- cleansam.sh
+++ cleansam.sh
@@ -5,8 +5,11 @@ source PROGRAMPATHS
 source DIRECTORIES
 
 INPUT=$1
+mkdir -p $CLEANDIR
+
 OUTPUT=`echo $INPUT | sed 's/\// /g' | awk '{print $NF}' | sed 's/\_^*[a-z]\w*.bam/\_clean.bam/g'`
 CLEAN=$CLEANDIR/$OUTPUT
-echo "Cleaning bam file: $INPUT"
-$PICARD CleanSam I=$INPUT O=$CLEAN
 
+echo "Cleaning file: $INPUT ..." 
+$PICARD CleanSam I=$INPUT O=$CLEAN
+echo "Done! Cleaining file: $INPUT"
