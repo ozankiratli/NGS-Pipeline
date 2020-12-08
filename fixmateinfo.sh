37a7ea522c9bcf1c0584e06189ea9f2ac75f1f33
Ozan Kiratli <ozankiratli@protonmail.com>
2020-12-08 16:43:19 -0500

    r tag removed, minor changes

--- fixmateinfo.sh
+++ fixmateinfo.sh
@@ -12,5 +12,6 @@ mkdir -p $FIXEDDIR
 OUTPUT=`echo $INPUT | sed 's/\// /g' | awk '{print $NF}' | sed 's/\_^*[a-z]\w*.bam/\_fixmate.bam/g'`
 FIXED=$FIXEDDIR/$OUTPUT
 
-echo "Fixing Mate Info for $INPUT ..."
-$SAMTOOLS fixmate -@ $SAMTOOLSCORES -O bam -rm $INPUT $FIXED
+echo "Fixing Mate Info: $INPUT ..."
+$SAMTOOLS fixmate -@ $SAMTOOLSCORES -O bam -m $INPUT $FIXED
+echo "Done! Fixing Mate Info: $INPUT..."
