ec0ff0278c6b513223a0f59993e0a2eb0a1ca414
Ozan Kiratli <ozankiratli@protonmail.com>
2020-12-08 16:43:39 -0500

    Minor change to add to serial preprocessing

--- coordsort.sh
+++ coordsort.sh
@@ -12,5 +12,6 @@ mkdir -p $SORTEDDIR
 OUTPUT=`echo $INPUT | sed 's/\// /g' | awk '{print $NF}' | sed 's/\_^*[a-z]\w*.bam/\_sorted.bam/g'`
 SORTED=$SORTEDDIR/$OUTPUT
 
-echo "Sorting $INPUT by coordinate..."
+echo "Sorting by coordinate: $INPUT ..."
 $SAMTOOLS sort -@ $SAMTOOLSCORES -O bam $INPUT -o $SORTED
+echo "Done! Sorting by coordinate: $INPUT"
