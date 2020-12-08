6b8e57c27ad0d106e9b775b5e73de3a80df38672
Ozan Kiratli <ozankiratli@protonmail.com>
2020-12-08 16:41:31 -0500

    Minor change to add to serial preprocessing

--- namesort.sh
+++ namesort.sh
@@ -12,6 +12,6 @@ mkdir -p $NSORTEDDIR
 OUTPUT=`echo $INPUT | sed 's/\// /g' | awk '{print $NF}' | sed 's/.sam/\_nsorted.bam/g'`
 NSORTED=$NSORTEDDIR/$OUTPUT
 
-echo "Sorting $INPUT by name..."
+echo "Sorting by name: $INPUT ..."
 $SAMTOOLS sort -@ $SAMTOOLSCORES -O bam -n $INPUT -o $NSORTED
-
+echo "Done! Sorting by name: $INPUT"
