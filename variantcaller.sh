3b1dd83ba077cd44a2c87c85ff0a63b0947e65a0
Ozan Kiratli <ozankiratli@protonmail.com>
2020-12-10 01:11:47 -0500

    alternative line

--- variantcaller.sh
+++ variantcaller.sh
@@ -16,3 +16,5 @@ wait
 echo "Done!"
 echo " "
 
+
+#bcftools mpileup -B -C50 -q20 -Q20 -d2000 -a "AD,DP,SP,INFO/AD" -Ou -f Reference/dmel_v5-39_all.fasta Output/ready/*.bam | bcftools call -m -A -v -Ov -o Output/VCF/bcftools_pd1210.vcf
