6010f91ff9c4815ce4ede7aa9ef3f2ef17fe78e5
Ozan Kiratli <ozankiratli@protonmail.com>
2020-12-11 22:53:39 -0500

    bcftools is used for variant calling instead of freebayes

--- variantcaller.sh
+++ variantcaller.sh
@@ -11,10 +11,9 @@ VCFFILE=$VCFDIR/$VCFFILEN
 
 echo "Creating VCF file: $VCFFILE ..."
 
-$FREEBAYES -f $REF $FREEBAYESOPTIONS $READYDIR/*.bam > $VCFFILE
+$BCFTOOLS mpileup $BCFMPILEUPOPTIONS -f $REF $READYDIR/*.bam | $BCFTOOLS call $BCFCALLOPTIONS -o $VCFFILE
+
 wait
 echo "Done!"
 echo " "
 
-
-#bcftools mpileup -B -C50 -q20 -Q20 -d2000 -a "AD,DP,SP,INFO/AD" -Ou -f Reference/dmel_v5-39_all.fasta Output/ready/*.bam | bcftools call -m -A -v -Ov -o Output/VCF/bcftools_pd1210.vcf
